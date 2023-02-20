052dee8744b5f13f6615350ea5d93a9273e4cf27
Rohan2407 <rohanshrivastava2407@gmail.com>
2023-02-20 17:02:30 +0530

    updated for canto

--- src/mappings/Turnstile.ts
+++ src/mappings/Turnstile.ts
@@ -1,139 +1,37 @@
 import {
-  Approval as ApprovalEvent,
-  ApprovalForAll as ApprovalForAllEvent,
   Assign as AssignEvent,
   DistributeFees as DistributeFeesEvent,
   OwnershipTransferred as OwnershipTransferredEvent,
   Register as RegisterEvent,
   Transfer as TransferEvent,
-  Withdraw as WithdrawEvent,
 } from "../../generated/Turnstile/Turnstile";
-import {
-  Approval,
-  ApprovalForAll,
-  Assign,
-  DistributeFees,
-  OwnershipTransferred,
-  Register,
-  Transfer,
-  Withdraw,
-} from "../../generated/schema";
-
-export function handleApproval(event: ApprovalEvent): void {
-  let entity = new Approval(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  );
-  entity.owner = event.params.owner;
-  entity.approved = event.params.approved;
-  entity.tokenId = event.params.tokenId;
-
-  entity.blockNumber = event.block.number;
-  entity.blockTimestamp = event.block.timestamp;
-  entity.transactionHash = event.transaction.hash;
-
-  entity.save();
-}
-
-export function handleApprovalForAll(event: ApprovalForAllEvent): void {
-  let entity = new ApprovalForAll(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  );
-  entity.owner = event.params.owner;
-  entity.operator = event.params.operator;
-  entity.approved = event.params.approved;
-
-  entity.blockNumber = event.block.number;
-  entity.blockTimestamp = event.block.timestamp;
-  entity.transactionHash = event.transaction.hash;
-
-  entity.save();
-}
+import { NFT } from "../../generated/schema";
 
 export function handleAssign(event: AssignEvent): void {
-  let entity = new Assign(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  );
-  entity.smartContract = event.params.smartContract;
-  entity.tokenId = event.params.tokenId;
-
-  entity.blockNumber = event.block.number;
-  entity.blockTimestamp = event.block.timestamp;
-  entity.transactionHash = event.transaction.hash;
-
-  entity.save();
-}
-
-export function handleDistributeFees(event: DistributeFeesEvent): void {
-  let entity = new DistributeFees(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  );
-  entity.tokenId = event.params.tokenId;
-  entity.feeAmount = event.params.feeAmount;
-
-  entity.blockNumber = event.block.number;
-  entity.blockTimestamp = event.block.timestamp;
-  entity.transactionHash = event.transaction.hash;
-
-  entity.save();
-}
-
-export function handleOwnershipTransferred(
-  event: OwnershipTransferredEvent
-): void {
-  let entity = new OwnershipTransferred(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  );
-  entity.previousOwner = event.params.previousOwner;
-  entity.newOwner = event.params.newOwner;
-
-  entity.blockNumber = event.block.number;
-  entity.blockTimestamp = event.block.timestamp;
-  entity.transactionHash = event.transaction.hash;
-
-  entity.save();
+  let entity = NFT.load(event.params.tokenId.toString());
+  let contractArray = entity!.contracts;
+  if (contractArray !== null) {
+    contractArray = contractArray.concat([event.params.smartContract]);
+  } else {
+    contractArray = [event.params.smartContract];
+  }
+  entity!.contracts = contractArray;
+  entity!.save();
 }
 
 export function handleRegister(event: RegisterEvent): void {
-  let entity = new Register(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  );
-  entity.smartContract = event.params.smartContract;
-  entity.recipient = event.params.recipient;
+  let entity = new NFT(event.params.tokenId.toString());
+  entity.owner = event.params.recipient;
+  entity.contracts = [event.params.smartContract];
   entity.tokenId = event.params.tokenId;
-
-  entity.blockNumber = event.block.number;
-  entity.blockTimestamp = event.block.timestamp;
-  entity.transactionHash = event.transaction.hash;
-
   entity.save();
 }
 
 export function handleTransfer(event: TransferEvent): void {
-  let entity = new Transfer(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  );
-  entity.from = event.params.from;
-  entity.to = event.params.to;
-  entity.tokenId = event.params.tokenId;
-
-  entity.blockNumber = event.block.number;
-  entity.blockTimestamp = event.block.timestamp;
-  entity.transactionHash = event.transaction.hash;
-
-  entity.save();
-}
-
-export function handleWithdraw(event: WithdrawEvent): void {
-  let entity = new Withdraw(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  );
-  entity.tokenId = event.params.tokenId;
-  entity.recipient = event.params.recipient;
-  entity.feeAmount = event.params.feeAmount;
-
-  entity.blockNumber = event.block.number;
-  entity.blockTimestamp = event.block.timestamp;
-  entity.transactionHash = event.transaction.hash;
-
+  let entity = NFT.load(event.params.tokenId.toString());
+  if (!entity) {
+    entity = new NFT(event.params.tokenId.toString());
+  }
+  entity.owner = event.params.to;
   entity.save();
 }
